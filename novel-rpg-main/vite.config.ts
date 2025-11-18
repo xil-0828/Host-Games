4737932abae62c8b59d6d4f6d5b541209e73cf08
xil-0828 <yuki.2002.828@icloud.com>
2025-11-18 18:19:58 +0900

    コミットチェンジ

--- novel-rpg-main/vite.config.ts
+++ novel-rpg-main/vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/novel-rpg/",
+  plugins: [react()], // ←これが絶対必要
   build: {
     outDir: "../public/novel-rpg",
     emptyOutDir: true,
-  }
+  },
 });
